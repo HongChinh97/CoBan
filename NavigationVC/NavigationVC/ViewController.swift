7243b4e9e71fb03c027a76f55b04b42ad90f81ab
admin <chiinhuv@gmail.com>
2018-11-24 16:03:47 +0700

    no message

---
+++ NavigationVC/NavigationVC/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NavigationVC
+//
+//  Created by admin on 11/22/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
